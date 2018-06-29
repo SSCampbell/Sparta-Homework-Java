cbd7868b45a991aef2d040421b0e1d2632f86f3a
unknown <Scampbell@spartaglobal.com>
2018-06-29 23:35:50 +0100

    CALCULATOR: The first prompt window was created for Calculator choice.

---
+++ main.js
@@ -0,0 +1,19 @@
+var b = "BASIC";
+var a = "ADVANCED";
+
+answer = prompt("What Calculator Would You Like To Use: (A) = ADVANCED (B)= BASIC");
+
+if (answer.toUpperCase() == "A")
+{
+alert("ADVANCED Calculator <Selected>");
+answer = prompt("What Function Would You Like To Perform: ");
+
+}
+else if (answer == "B")
+{
+alert("BASIC Calculator <Selected>");
+}
+else
+{
+
+}
